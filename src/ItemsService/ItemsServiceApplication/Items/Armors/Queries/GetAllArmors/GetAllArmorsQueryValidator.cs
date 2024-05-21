0fd6522b2ec30beefe3552ab2886fd1720994ebe
artem <artem.sorkin.v@gmail.com>
2024-05-21 08:54:10 +0500

    Add weapons and armors pagination validators

---
+++ src/ItemsService/ItemsServiceApplication/Items/Armors/Queries/GetAllArmors/GetAllArmorsQueryValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace ItemsService.ItemsServiceApplication.Items.Armors.Queries.GetAllArmors;
+
+public class GetAllArmorsQueryValidator : AbstractValidator<GetAllArmorsQuery>
+{
+    private readonly int[] _allowedPageSizes = [4, 8, 12];
+    
+    public GetAllArmorsQueryValidator()
+    {
+        RuleFor(r => r.PageNumber).GreaterThanOrEqualTo(1);
+        
+        RuleFor(r => r.PageSize)
+            .Must(p => _allowedPageSizes.Contains(p))
+            .WithMessage($"Page size must in [{string.Join(",", _allowedPageSizes)}]");
+    }
+}
---
+++ src/ItemsService/ItemsServiceApplication/Items/Weapons/Queries/GetAllWeapons/GetAllWeaponsQueryValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace ItemsService.ItemsServiceApplication.Items.Weapons.Queries.GetAllWeapons;
+
+public class GetAllWeaponsQueryValidator : AbstractValidator<GetAllWeaponsQuery>
+{
+    private readonly int[] _allowedPageSizes = [4, 8, 12];
+    
+    public GetAllWeaponsQueryValidator()
+    {
+        RuleFor(r => r.PageNumber).GreaterThanOrEqualTo(1);
+        
+        RuleFor(r => r.PageSize)
+            .Must(p => _allowedPageSizes.Contains(p))
+            .WithMessage($"Page size must in [{string.Join(",", _allowedPageSizes)}]");
+    }
+}
