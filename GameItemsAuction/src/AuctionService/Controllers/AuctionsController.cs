792484a175382a5ca8721f8bfd40e3c2fd8eb83d
artem <artem.sorkin.v@gmail.com>
2024-05-23 08:21:59 +0500

    Add basic controller with GetAllAuctions and GetAuctionsById

--- GameItemsAuction/src/AuctionService/Controllers/AuctionsController.cs
+++ GameItemsAuction/src/AuctionService/Controllers/AuctionsController.cs
@@ -1,11 +1,44 @@
-using Microsoft.AspNetCore.Http;
+using AuctionService.Data;
+using AuctionService.DTO;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
-namespace AuctionService.Controllers
+namespace AuctionService.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuctionsController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class AuctionsController : ControllerBase
+    private readonly AuctionDbContext _context;
+    private readonly IMapper _mapper;
+
+    public AuctionsController(AuctionDbContext context, IMapper mapper)
     {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<AuctionDto>>> GetAllAuctions()
+    {
+        var auctions = await _context.Auctions
+            .Include(x => x.Item)
+            .OrderBy(x => x.Item.ItemDbId)
+            .ToListAsync();
+
+        return _mapper.Map<List<AuctionDto>>(auctions);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<AuctionDto>> GetAuctionById(Guid id)
+    {
+        var auction = await _context.Auctions
+            .Include(x => x.Item)
+            .FirstOrDefaultAsync(x => x.Id == id);
+
+        if (auction == null) return NotFound();
+        
+        return _mapper.Map<AuctionDto>(auction);
     }
 }
